e4a5016d8ead1be324b6b387b0a673b1429a7246
TetsuFe <tetsubeg1ner27@gmail.com>
2019-03-20 13:30:34 +0900

    Add a form on bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -64,15 +64,66 @@ class MyHomePage extends StatelessWidget {
       return new Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          new ListTile(
-            leading: new Icon(Icons.music_note),
-            title: new Text('Music'),
-            onTap: () => {},
-          ),
-          new RaisedButton(onPressed: () => bloc.charge(300)),
+          new MyCustomForm()
         ],
       );
     });
   }
 }
 
+// Create a Form Widget
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that will uniquely identify the Form widget and allow
+  // us to validate the form
+  //
+  // Note: This is a GlobalKey<FormState>, not a GlobalKey<MyCustomFormState>!
+  final _formKey = GlobalKey<FormState>();
+
+  int _chargeMoney = 0;
+
+  submit(bloc){
+    _formKey.currentState.save();
+    bloc.charge(_chargeMoney);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = BlocProvider.of(context);
+    // Build a Form widget using the _formKey we created above
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.number,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+            },
+            onSaved: (String value) => this._chargeMoney = int.parse(value),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                this.submit(bloc);
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
