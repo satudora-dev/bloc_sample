ad2d0426664222726a8f97c9bacb80fc04cd5005
TetsuFe <tetsubeg1ner27@gmail.com>
2019-03-27 17:12:52 +0900

    Bug report snapshot of failure of close modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bloc_sample/charge_bloc_provider.dart';
 import 'package:bloc_sample/charge_bloc.dart';
+import 'package:bloc_sample/charge_form_dialog.dart';
 
 void main() {
   final bloc = ChargeBloc();
@@ -44,91 +45,38 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             StreamBuilder(
                 stream: bloc.chargedMoney$,
-                builder: (context, snapshot) =>
-                  snapshot.hasData
-                      ? Text('${snapshot.data}',
-                      style: Theme
-                          .of(context)
-                          .textTheme
-                          .display1)
-                      : CircularProgressIndicator())
-                ,
+                builder: (context, snapshot) => snapshot.hasData
+                    ? Text('${snapshot.data}',
+                        style: Theme.of(context).textTheme.display1)
+                    : CircularProgressIndicator()),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => showModal(context, bloc),
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      floatingActionButton: ButtonToChargeFormDialog(),
     );
   }
-
-  showModal(context, bloc){
-    showModalBottomSheet<void>(context: context, builder: (BuildContext context) {
-      return new Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          new MyCustomForm()
-        ],
-      );
-    });
-  }
 }
 
-// Create a Form Widget
-class MyCustomForm extends StatefulWidget {
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that will uniquely identify the Form widget and allow
-  // us to validate the form
-  //
-  // Note: This is a GlobalKey<FormState>, not a GlobalKey<MyCustomFormState>!
-  final _formKey = GlobalKey<FormState>();
-
-  int _chargeMoney = 0;
-
-  submit(bloc){
-    _formKey.currentState.save();
-    bloc.addMoney.add(_chargeMoney);
-  }
-
+class ButtonToChargeFormDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey we created above
     final bloc = BlocProvider.of(context);
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-            },
-            onSaved: (String value) => this._chargeMoney = int.parse(value),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                this.submit(bloc);
-              },
-              child: Text('Submit'),
-            ),
-          ),
-        ],
-      ),
-    );
+    return FloatingActionButton(
+        onPressed: () => showModal(context, bloc),
+        tooltip: 'Increment',
+        child: Icon(Icons.add));
+  }
+
+  showModal(context, bloc) {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ChargeFormDialog()
+            ],
+          );
+        });
   }
 }
