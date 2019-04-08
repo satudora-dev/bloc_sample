ed814bec2fb5cb16e3c8ec5720ed80797cb04c41
sono8stream <sono.stream8580@gmail.com>
2019-04-08 10:06:39 +0900

    Remove animation and add animated container

--- lib/merpay_modal/bloc_charging_view.dart
+++ lib/merpay_modal/bloc_charging_view.dart
@@ -4,106 +4,53 @@ import 'dart:async';
 
 import './merpay_modal_bloc.dart';
 
-class BlocChargingView extends StatefulWidget {
-  @override
-  BlocChargingViewState createState() => BlocChargingViewState();
-}
-
-class BlocChargingViewState extends State<BlocChargingView>
-    with TickerProviderStateMixin {
-  AnimationController fadeController;
-  Animation<double> fadeAnimation;
-
-  StreamSubscription _streamSubscription;
-
-  MerpayModalBloc _bloc;
-
-  @override
-  void initState() {
-    super.initState();
-    fadeController = AnimationController(
-      duration: Duration(milliseconds: 2000),
-      vsync: this,
-    );
-    fadeAnimation = CurvedAnimation(
-      parent: fadeController,
-      curve: Curves.easeIn,
-    );
-
-    fadeController.addStatusListener((status) {
-      if (status == AnimationStatus.dismissed) {
-        final bloc = BlocProvider.of<MerpayModalBloc>(context).chargingBloc;
-        bloc.completeCharge.add(null);
-      }
-    });
-
-    _bloc = BlocProvider.of<MerpayModalBloc>(context);
-    _streamSubscription = _bloc.animationCompleted.listen((value) {
-      startAnimation();
-    });
-  }
-
-  startAnimation() {
-    final status = fadeAnimation.status;
-    debugPrint(status.toString());
-    if (status == AnimationStatus.dismissed) {
-      fadeController.forward();
-    } else if (status == AnimationStatus.completed) {
-      fadeController.reverse();
-    }
-  }
-
-  @override
-  void dispose() {
-    _streamSubscription.cancel();
-    super.dispose();
-  }
+class BlocChargingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    MerpayModalBloc bloc = BlocProvider.of<MerpayModalBloc>(context);
+
     return StreamBuilder(
-        stream: _bloc.chargingViewVisible,
+        stream: bloc.chargingViewVisible,
         initialData: false,
         builder: (context, snap) {
           if (snap.data) {
             return Positioned.fill(
-              child: Container(
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500,),
                 color: Color.fromARGB(100, 0, 0, 0),
-                child: FadeTransition(
-                  opacity: fadeAnimation,
-                  child: Padding(
-                    padding: EdgeInsets.all(100.0),
-                    child: Container(
-                      color: Colors.amber[100],
-                      child: Column(
-                        children: <Widget>[
-                          Text('チャージしています'),
-                          RaisedButton(
-                            child: Text('Done'),
-                            onPressed: () {
-                              //bloc.completeCharge.add(null);
-                              _bloc.completeAnimation.add(null);
-                            },
-                          ),
-                          RaisedButton(
-                            child: Text('Change Color'),
-                            onPressed: () {
-                              _bloc.chargingBloc.changeColor
-                                  .add(Color.fromARGB(100, 0, 0, 0));
-                            },
-                          ),
-                          StreamBuilder(
-                            stream: _bloc.chargingBloc.color,
-                            builder: (context, snap) => AnimatedContainer(
-                                  padding: EdgeInsets.all(50),
-                                  duration: Duration(
-                                    milliseconds: 1000,
-                                  ),
-                                  color: snap.data,
+                child: Padding(
+                  padding: EdgeInsets.all(100.0),
+                  child: Container(
+                    color: Colors.amber[100],
+                    child: Column(
+                      children: <Widget>[
+                        Text('チャージしています'),
+                        RaisedButton(
+                          child: Text('Done'),
+                          onPressed: () {
+                            //bloc.completeCharge.add(null);
+                            bloc.completeAnimation.add(null);
+                          },
+                        ),
+                        RaisedButton(
+                          child: Text('Change Color'),
+                          onPressed: () {
+                            bloc.chargingBloc.changeColor
+                                .add(Color.fromARGB(100, 0, 0, 0));
+                          },
+                        ),
+                        StreamBuilder(
+                          stream: bloc.chargingBloc.color,
+                          builder: (context, snap) => AnimatedContainer(
+                                padding: EdgeInsets.all(50),
+                                duration: Duration(
+                                  milliseconds: 1000,
                                 ),
-                          ),
-                        ],
-                      ),
+                                color: snap.data,
+                              ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
