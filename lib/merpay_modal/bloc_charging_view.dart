42185d6de7001fbda5ea64927771f07f5c59ba01
sono8stream <sono.stream8580@gmail.com>
2019-04-08 10:22:42 +0900

    Change charging view animation to animated opacity

--- lib/merpay_modal/bloc_charging_view.dart
+++ lib/merpay_modal/bloc_charging_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:bloc_provider/bloc_provider.dart';
-import 'dart:async';
 
 import './merpay_modal_bloc.dart';
 
 class BlocChargingView extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     MerpayModalBloc bloc = BlocProvider.of<MerpayModalBloc>(context);
@@ -14,51 +12,55 @@ class BlocChargingView extends StatelessWidget {
         stream: bloc.chargingViewVisible,
         initialData: false,
         builder: (context, snap) {
-          if (snap.data) {
-            return Positioned.fill(
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 500,),
-                color: Color.fromARGB(100, 0, 0, 0),
-                child: Padding(
-                  padding: EdgeInsets.all(100.0),
-                  child: Container(
-                    color: Colors.amber[100],
-                    child: Column(
-                      children: <Widget>[
-                        Text('チャージしています'),
-                        RaisedButton(
-                          child: Text('Done'),
-                          onPressed: () {
-                            //bloc.completeCharge.add(null);
-                            bloc.completeAnimation.add(null);
-                          },
-                        ),
-                        RaisedButton(
-                          child: Text('Change Color'),
-                          onPressed: () {
-                            bloc.chargingBloc.changeColor
-                                .add(Color.fromARGB(100, 0, 0, 0));
-                          },
-                        ),
-                        StreamBuilder(
-                          stream: bloc.chargingBloc.color,
-                          builder: (context, snap) => AnimatedContainer(
-                                padding: EdgeInsets.all(50),
-                                duration: Duration(
-                                  milliseconds: 1000,
+          return Positioned.fill(
+            child: IgnorePointer(
+              ignoring: !snap.data,
+              child: AnimatedOpacity(
+                duration: Duration(
+                  milliseconds: 500,
+                ),
+                opacity: snap.data ? 1 : 0,
+                child: Container(
+                  color: Color.fromARGB(100, 0, 0, 0),
+                  child: Padding(
+                    padding: EdgeInsets.all(100.0),
+                    child: Container(
+                      color: Colors.amber[100],
+                      child: Column(
+                        children: <Widget>[
+                          Text('チャージしています'),
+                          RaisedButton(
+                            child: Text('Done'),
+                            onPressed: () {
+                              //bloc.completeCharge.add(null);
+                              bloc.completeAnimation.add(null);
+                            },
+                          ),
+                          RaisedButton(
+                            child: Text('Change Color'),
+                            onPressed: () {
+                              bloc.chargingBloc.changeColor
+                                  .add(Color.fromARGB(100, 0, 0, 0));
+                            },
+                          ),
+                          StreamBuilder(
+                            stream: bloc.chargingBloc.color,
+                            builder: (context, snap) => AnimatedContainer(
+                                  padding: EdgeInsets.all(50),
+                                  duration: Duration(
+                                    milliseconds: 1000,
+                                  ),
+                                  color: snap.data,
                                 ),
-                                color: snap.data,
-                              ),
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            );
-          } else {
-            return Container();
-          }
+            ),
+          );
         });
   }
 }
