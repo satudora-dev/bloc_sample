bbc2224ee0aa08f6269093c66077e3db18a16321
TetsuFe <tetsubeg1ner27@gmail.com>
2019-04-19 04:49:14 +0900

    Add test script for save and get health profile data

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import './connection_bloc.dart';
 import './merpay_modal/merpay_modal_bloc.dart';
 import './merpay_modal/bloc_merpay_view.dart';
 
+import './Model/HealthProfile.dart';
+import './Repository/HealthProfileRepository.dart';
+
 void main() => runApp(
       BlocProviderTree(
         blocProviders: [
@@ -20,6 +23,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final healthProfileRepository = HealthProfileRepository();
+    healthProfileRepository.reset();
+    healthProfileRepository.save(HealthProfile(DateTime.now(), "60", "36.0"));
+    healthProfileRepository.save(HealthProfile(DateTime.now(), "70", "37.0"));
+    healthProfileRepository.save(HealthProfile(DateTime.now(), "80", "38.0"));
+    healthProfileRepository.getAllHealthProfile();
     final connectionBloc = BlocProvider.of<ConnectionBloc>(context);
     return MaterialApp(
       title: 'Flutter Demo',
