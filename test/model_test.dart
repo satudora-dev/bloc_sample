a8a253484f478c7d7a944e6731a62eb82e40cb33
TetsuFe <tetsubeg1ner27@gmail.com>
2019-04-19 05:52:18 +0900

    Delete debug print in main.dart and add test instead of the debug

---
+++ test/model_test.dart
@@ -0,0 +1,32 @@
+import "package:flutter_test/flutter_test.dart";
+
+import '../lib/Model/HealthProfile.dart';
+import '../lib/Repository/HealthProfileRepository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+    final healthProfileRepository = HealthProfileRepository();
+    healthProfileRepository.reset();
+  });
+
+  test("health profileデータのshared_preferenceへの保存・取り出し", () async {
+    final healthProfileRepository = HealthProfileRepository();
+    final healthProfileList = [
+      HealthProfile(DateTime.now(), "60", "36.0"),
+      HealthProfile(DateTime.now(), "70", "37.0"),
+      HealthProfile(DateTime.now(), "80", "38.0")
+    ];
+    for (var t in healthProfileList) {
+      healthProfileRepository.save(t);
+    }
+    final allHealthProfileList =
+        await healthProfileRepository.getAllHealthProfile();
+    for (var i = 0; i < allHealthProfileList.length; i++) {
+      expect(allHealthProfileList[i].height, equals(healthProfileList[i].height));
+      expect(allHealthProfileList[i].date, equals(healthProfileList[i].date));
+      expect(allHealthProfileList[i].bodyTemperature, equals(healthProfileList[i].bodyTemperature));
+    }
+  });
+}
