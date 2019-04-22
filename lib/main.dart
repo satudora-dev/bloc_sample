da03bc8cf85455d1135acce468993203207881e6
TetsuFe <tetsubeg1ner27@gmail.com>
2019-04-22 09:49:10 +0900

    Add home page to create new context and don't use navigate custom button

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:bloc_provider/bloc_provider.dart';
 
 import './connection_bloc.dart';
-import './merpay_modal/go_to_merpay_trace_button.dart';
-import './Calendar/GoToCalendarButton.dart';
+import './merpay_modal/merpay_trace_screen.dart';
+import './Calendar/CalendarSelectScreen.dart';
 
 void main() => runApp(
       BlocProviderTree(
@@ -20,25 +20,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final connectionBloc = BlocProvider.of<ConnectionBloc>(context);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final connectionBloc = BlocProvider.of<ConnectionBloc>(context);
+    return Scaffold(
         appBar: AppBar(title: Text('Bloc Test')),
         body: Column(children: <Widget>[
-          GoToMerpayTraceButton(),
-          ButtonToCalendarScreen()
+          RaisedButton(
+              child: Text("メルペイトレース"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MerpayTraceScreen()),
+                );
+              }),
+          RaisedButton(
+              child: Text("カレンダー"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => CalendarSelectScreen()),
+                );
+              })
         ]),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
             connectionBloc.counterBloc.increment.add(null);
           },
-        ),
-      ),
-    );
+        ));
   }
 }
